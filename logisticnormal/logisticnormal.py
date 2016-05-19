a80246880fa13c0d0128b2f3ec304d640619388c
Lisa Koch <lisa.margret.koch@gmail.com>
2016-05-19 14:28:40 +0100

    Transposed everything to assumed data shape is (N,D), where N is the number of samples. This conforms much better with the rest of the world.

--- logisticnormal/logisticnormal.py
+++ logisticnormal/logisticnormal.py
@@ -16,9 +16,9 @@ def fit(data):
     Estimate parameters of logistic normal distribution by estimating the multivariate gaussian distribution of the
     logit-transformed data
 
-    :param data:    array, shape (D,N)
-                    D: number of categories (dimensions),
-                    N: number of samples
+    :param data:    array, shape (N,D)
+                    N: number of samples,
+                    D: number of categories (dimensions)
     :return: mu and cov: parameters of the logistic normal distribution
     """
 
@@ -27,12 +27,12 @@ def fit(data):
     # transform the data with the logit function
     # y = [log(x1/xD), ..., log(x_{D-1}/xD)]
 
-    # divide all values by last row and apply log
-    y = np.log(data[:-1,:] / data[-1,:])
+    # divide all values by last column and apply log
+    y = np.log(data[:,:-1] / data[:,-1])
 
     # calculate the mean and covariance of transformed data
-    mu = np.average(y, axis=1)
-    covar = np.cov(y)
+    mu = np.average(y, axis=0)
+    covar = np.cov(y.T)
 
     return mu, covar
 
@@ -46,23 +46,26 @@ def pdf(mu, covar):
     :return: logisticnormal function handle
     """
 
-    D = mu.shape[0] + 1
+    mu = np.atleast_1d(mu)
+    covar = np.atleast_2d(covar)
+
+    D = len(np.ravel(mu)) + 1
 
     if covar.shape != (D-1,D-1):
         raise Exception('Parameter dimension mismatch. mu.shape is {}, covar.shape is {}'.format(mu.shape, covar.shape))
 
     def logisticnormal(x):
         """
-        :param x:   array, shape (D,N)
-                    D: number of categories (dimensions),
-                    N: number of samples
+        :param x:   array, shape (N,D)
+                    N: number of samples,
+                    D: number of categories (dimensions)
         :return: f(x)
         """
 
-        if x.shape[0] != D:
+        if x.shape[1] != D:
             raise Exception('Data dimension mismatch. Number of rows should be {}, but is {}'.format(D, x.shape[0]))
 
-        y = np.log(x[:-1,:] / x[-1,:]).T
+        y = np.log(x[:,:-1] / x[:,-1])
 
         # apply multivariate normal pdf
         return multivariate_normal.pdf(y, mean=mu, cov=covar, allow_singular=True)
