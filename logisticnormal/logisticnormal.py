Preserve array dimensions properly. Also fixed a bug in the pdf (wrong normalisation).

logisticnormal/logisticnormal.py
@@ -28,7 +28,7 @@ def fit(data):
     # y = [log(x1/xD), ..., log(x_{D-1}/xD)]
 
     # divide all values by last column and apply log
-    y = np.log(data[:,:-1] / data[:,-1])
+    y = np.log(data[:,:-1] / data[:,-1][:, None])
 
     # calculate the mean and covariance of transformed data
     mu = np.average(y, axis=0)
@@ -65,10 +65,30 @@ def pdf(mu, covar):
         if x.shape[1] != D:
             raise Exception('Data dimension mismatch. Number of rows should be {}, but is {}'.format(D, x.shape[0]))
 
-        y = np.log(x[:,:-1] / x[:,-1])
+        y = np.log(x[:,:-1] / (x[:,-1][:, None]+0.0000001))
 
         # apply multivariate normal pdf
         return multivariate_normal.pdf(y, mean=mu, cov=covar, allow_singular=True)
 
+
+    def logisticnormal_corrected(x):
+        """ Corrected version - I suspect the version above is wrong
+
+        :param x:   array, shape (N,D)
+                    N: number of samples,
+                    D: number of categories (dimensions)
+        :return: f(x)
+        """
+
+        if x.shape[1] != D:
+            raise Exception('Data dimension mismatch. Number of rows should be {}, but is {}'.format(D, x.shape[0]))
+
+        y = np.log(x[:,:-1] / (x[:,-1][:, None]+0.0000001))
+
+        # apply multivariate normal pdf
+        return multivariate_normal.pdf(y, mean=mu, cov=covar, allow_singular=True) / np.prod(x, axis=1)
+
+
+    return logisticnormal_corrected
     return logisticnormal
 